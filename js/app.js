64e4425d7635e4acab0a677e835407f50afe5ced
rollcloud <rollcloud@gmail.com>
2024-01-27 14:47:54 +0000

    refactor: stylesheets

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
 import "/node_modules/flag-icons/css/flag-icons.min.css";
-import "../stylesheets/icons.scss";
-import "../stylesheets/maps.scss";
 import "../stylesheets/style.scss";
 import "./script.js";
